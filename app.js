b112f05c2a1f8dacea332ccb0fa9aa4d552a244d
frarosset <37275581+frarosset@users.noreply.github.com>
2025-06-25 17:16:24 +0200

    feat(app): use express.urlencoded middleware to parse form data into req.body

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 
+const PORT = process.env.PORT || 80;
+
 const indexRouter = require("./routes/indexRouter.js");
 const newRouter = require("./routes/newRouter.js");
 
@@ -9,7 +11,7 @@ const app = express();
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-const PORT = process.env.PORT || 80;
+app.use(express.urlencoded({ extended: true })); // to parse form data into req.body
 
 app.use("/new", newRouter);
 app.use(indexRouter);
