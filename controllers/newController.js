aa809788d7f3b16710e5a99a18f2808e6139d43c
frarosset <37275581+frarosset@users.noreply.github.com>
2025-06-28 12:23:20 +0200

    feat(newController): add content moderation to new posted message using Azure AI Content Safety
    Harmful messages are blocked and not published.

--- controllers/newController.js
+++ controllers/newController.js
@@ -1,12 +1,27 @@
 const { addMessage } = require("../messagesDb.js");
-
+const textModeration = require("../utils/textModeration.js");
 const getNewController = (req, res) => res.render("form");
 
-const postNewController = (req, res) => {
+const postNewController = async (req, res, next) => {
   const { text, user } = req.body;
 
-  addMessage(text, user);
-  res.redirect("/");
+  try {
+    // apply moderation
+    const moderationOutput = await textModeration(`${user}: ${text}`);
+    console.log(moderationOutput);
+
+    // filter out harmful content
+    if (moderationOutput.isHarmful) {
+      res.render("form");
+      return;
+    }
+
+    // add message to board
+    addMessage(text, user);
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { get: getNewController, post: postNewController };
---
+++ utils/textModeration.js
@@ -0,0 +1,48 @@
+const ContentSafetyClient = require("@azure-rest/ai-content-safety").default,
+  { isUnexpected } = require("@azure-rest/ai-content-safety");
+const { AzureKeyCredential } = require("@azure/core-auth");
+
+// get endpoint and key from environment variables
+const endpoint = process.env.CONTENT_SAFETY_ENDPOINT;
+const key = process.env.CONTENT_SAFETY_KEY;
+const minHarmfulSeverityLevel = process.env.MIN_HARMFUL_SEVERITY_LEVEL;
+
+const credential = new AzureKeyCredential(key);
+const client = ContentSafetyClient(endpoint, credential);
+
+async function textModeration(text) {
+  const analyzeTextOption = { text: text };
+  const analyzeTextParameters = { body: analyzeTextOption };
+
+  const result = await client.path("/text:analyze").post(analyzeTextParameters);
+
+  if (isUnexpected(result)) {
+    const err = new Error(
+      `${result.body.error.message} [Azure-rest/ai-content-safety]`
+    );
+    err.code = result.body.error.code;
+    throw err;
+  }
+
+  const isHarmful = result.body.categoriesAnalysis.reduce((isHarmful, item) => {
+    return isHarmful || item.severity >= minHarmfulSeverityLevel;
+  }, false);
+
+  const outcome = {
+    categoriesAnalysis: result.body.categoriesAnalysis,
+    isHarmful,
+  };
+
+  //   for (let i = 0; i < result.body.categoriesAnalysis.length; i++) {
+  //     const textCategoriesAnalysisOutput = result.body.categoriesAnalysis[i];
+  //     console.log(
+  //       textCategoriesAnalysisOutput.category,
+  //       " severity: ",
+  //       textCategoriesAnalysisOutput.severity
+  //     );
+  //   }
+
+  return outcome;
+}
+
+module.exports = textModeration;
