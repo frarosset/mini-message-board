e8bb912105f181dc2d344f556fbba59521d9321b
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-01 01:12:28 +0200

    feat(messagesDb): implement function to get message data by its id

--- messagesDb.js
+++ messagesDb.js
@@ -2,6 +2,7 @@ const newMessage = (text = "", user = "(anonymous)") => ({
   text,
   user,
   added: new Date(),
+  id: crypto.randomUUID(),
 });
 
 const messages = [
@@ -10,6 +11,13 @@ const messages = [
 ];
 
 const getMessages = () => messages;
+const getMessage = (id) => {
+  const msg = messages.filter((msg) => {
+    return msg.id === id;
+  });
+
+  return msg !== null ? msg[0] : null;
+};
 const addMessage = (text, user) => messages.push(newMessage(text, user));
 
-module.exports = { getMessages, addMessage };
+module.exports = { getMessages, addMessage, getMessage };
