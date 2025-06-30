a68b144a783bafed3c03bd66114fe1ce6a669e4d
frarosset <37275581+frarosset@users.noreply.github.com>
2025-06-30 14:11:08 +0200

    feat: setup views to use styles from public folder

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 app.set("views", "views");
 app.set("view engine", "ejs");
 
+app.use(express.static("public", { extensions: ["html"] }));
+
 app.use(express.urlencoded({ extended: true })); // to parse form data into req.body
 
 app.use("/new", newRouter);
