880d5b0844fa5b709fc62c4e2d1f4868b9333bb6
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-01 11:01:00 +0200

    feat: implement error management in app server

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const PORT = process.env.PORT || 80;
 const indexRouter = require("./routes/indexRouter.js");
 const newRouter = require("./routes/newRouter.js");
 const messageRouter = require("./routes/messageRouter.js");
+const CustomNotFoundError = require("./errors/CustomNotFoundError");
 
 const app = express();
 
-app.set("views", "views");
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static("public", { extensions: ["html"] }));
@@ -19,5 +21,16 @@ app.use(express.urlencoded({ extended: true })); // to parse form data into req.
 app.use("/new", newRouter);
 app.use("/message", messageRouter);
 app.use(indexRouter);
+
+// catch-all route throwing a 404 error
+app.use((req, res, next) => {
+  throw new CustomNotFoundError("Page not found");
+});
+
+// Error handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(error.statusCode || 500).send(error.message);
+});
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
