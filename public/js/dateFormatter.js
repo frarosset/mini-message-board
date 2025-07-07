66fff0c48d7f6af03b5e6650abe60e5be626c6c2
frarosset <37275581+frarosset@users.noreply.github.com>
2025-07-07 12:12:04 +0200

    feat: export dateFormatter function from dateFormatter module

--- public/js/dateFormatter.js
+++ public/js/dateFormatter.js
@@ -1,13 +1,19 @@
 import dateToStr from "/js/dateToStr.js";
 
-const allDates = document.querySelectorAll(".date");
+function dateFormatter() {
+  const allDates = document.querySelectorAll(".date");
 
-allDates.forEach((el) => {
-  el.textContent = dateToStr(new Date(el.dataset.date));
-});
+  allDates.forEach((el) => {
+    el.textContent = dateToStr(new Date(el.dataset.date));
+  });
 
-const allSimpleDates = document.querySelectorAll(".simple-date");
+  const allSimpleDates = document.querySelectorAll(".simple-date");
 
-allSimpleDates.forEach((el) => {
-  el.textContent = dateToStr(new Date(el.dataset.date), "simple");
-});
+  allSimpleDates.forEach((el) => {
+    el.textContent = dateToStr(new Date(el.dataset.date), "simple");
+  });
+}
+
+dateFormatter();
+
+window.dateFormatter = dateFormatter;
